ae8c98693a11349880bd38478f14e1dc5e5162b4
David Gomez <davidgomez2924@gmail.com>
2021-12-09 18:19:33 -0300

    Desafio 6 Arrays, Array.prototype.sort() & Avance Proyecto final

--- src/js/script.js
+++ src/js/script.js
@@ -1,139 +1,121 @@
-const dineroElement = document.getElementById("dinero");
+const generate = document.getElementById("generate");
+const designSystem = document.getElementById("designSystem");
+const colors = document.getElementById("colors");
+const [primary, secondary, grays, state] = colors.children;
+const typography = document.getElementById("typography");
+const spacing = document.getElementById("spacing");
 
-let dinero;
-do {
-  dinero = parseInt(prompt("Ingresa el dinero"));
-} while (isNaN(dinero) || dinero < 0);
 
-dineroElement.textContent = dinero;
 
-function Product({ id, name, price, quantity, imageSrc }) {
-  // Creating DOM elements
-  /*
-    product
-      image
-      footer
-        id title price stock
-  */
-  const productContainer = document.createElement("div");
-  const footer = document.createElement("footer");
-  const idTag = document.createElement("span");
-  const title = document.createElement("span");
-  const priceTag = document.createElement("span");
-  const stock = document.createElement("span");
+/*
+ Response from the API is a JSON object with the following structure:
+{
+    hex: "#CCFB7B",
+    rgb: "rgb(204, 251, 123)",
+    hsl: "hsl(82, 51%, 98%)"
+}
+*/
 
-  // Adding attributes to DOM elements
-  const image = document.createElement("img");
-  image.src = imageSrc;
-
-  idTag.textContent = `ID: ${id}`;
-  title.textContent = `Name: ${name}`;
-  priceTag.textContent = `Price:$${price}`;
-  stock.textContent = `Stock: ${quantity}`;
-
-  // Adding Tailwind classes to DOM elements
-  productContainer.classList.add(
-    "flex",
-    "flex-col",
-    "items-center",
-    "rounded",
-    "cursor-pointer",
-    "flex-grow",
-    "overflow-hidden",
-    "shadow"
-  );
-
-  image.classList.add("object-cover", "h-full", "w-full");
-  idTag.classList.add(
-    "text-center",
-    "text-gray-700",
-    "font-bold",
-    "text-sm"
-  );
-  title.classList.add(
-    "text-center",
-    "text-gray-700",
-    "font-bold",
-    "text-sm"
-  );
-  priceTag.classList.add(
-    "text-center",
-    "text-green-400",
-    "font-bold",
-    "text-sm"
-  );
-
-  footer.classList.add(
-    "flex",
-    "flex-col",
-    "gap-2",
-    "bg-white",
-    "w-full"
-  );
-
-  quantity == 0
-    ? stock.classList.add("text-red-500")
-    : stock.classList.add("text-blue-500");
-  stock.classList.add(
-    "text-center",
-    "font-bold",
-    "text-sm"
-  );
-
-  // append DOM elements
-  footer.appendChild(idTag);
-  footer.appendChild(title);
-  footer.appendChild(priceTag);
-  footer.appendChild(stock);
-  productContainer.appendChild(image);
-  productContainer.appendChild(footer);
-
-  // function event to buy product
-  function buy() {
-    if (quantity <= 0) {
-      quantity = 0;
-      stock.classList.remove("text-blue-500");
-      stock.classList.add("text-red-500");
-
-      image.classList.add("filter", "grayscale");
-      alert("Producto fuera de stock");
-    } else if (dinero < price) {
-      alert("No tienes suficiente dinero");
-    } else {
-      dinero = dinero - price;
-      dineroElement.textContent = dinero;
-      alert(`Compraste ${name}`);
-      quantity--;
-    }
-
-    stock.textContent = `Stock: ${quantity}`;
+async function getRandomColors(range = 1) {
+  let data;
+  if (typeof range != "number") {
+    return console.error("Range must be a number");
   }
-  // adding event listeners
-  productContainer.addEventListener("click", buy);
 
-  return productContainer;
+  if (range <= 1) {
+    const url = `https://x-colors.herokuapp.com/api/random`;
+    const response = await fetch(url);
+    data = await response.json();
+  } else {
+    const url = `https://x-colors.herokuapp.com/api/random?number=${range}`;
+    const response = await fetch(url);
+    data = await response.json();
+  }
+  return data;
 }
 
-async function VendingMachine(cantidadProductos) {
-  const products = document.getElementById("products");
+// funciones que no use, pero que puedo usar mas adelante
+// async function setBgColor(element, format, colorToApply) {
+//   const color = colorToApply ?? (await getRandomColors());
+//   if (format === "hex" || format === "rgb" || format === "hsl") {
+//     element.style.backgroundColor =
+//       typeof color == "object" ? color[format] : color;
+//   } else {
+//     element.style.backgroundColor =
+//       typeof color == "object" ? color?.hex ?? color?.rgb ?? color?.hsl : color;
+//   }
+// }
 
-  for (let i = 0; i < cantidadProductos; i++) {
-    await fetch(
-      "https://random-data-api.com/api/food/random_food"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const product = Product({
-          id: data.id,
-          name: data.dish,
-          price: Math.floor(Math.random() * 100),
-          quantity: Math.floor(Math.random() * 10),
-          imageSrc: `https://picsum.photos/200/200/?random=${i}`,
-        });
+// async function setColor(element, format, colorToApply) {
+//   const color = colorToApply ?? (await getRandomColors());
+//   if (format === "hex" || format === "rgb" || format === "hsl") {
+//     element.style.color = typeof color == "object" ? color[format] : color;
+//   } else {
+//     element.style.color =
+//       typeof color == "object" ? color?.hex ?? color?.rgb ?? color?.hsl : color;
+//   }
+// }
 
-        products.appendChild(product);
-      });
+function ColorValues(colors) {
+  const column = document.createElement("div");
+  column.style.backgroundColor = colors.hex;
+  column.classList.add("space-y-2", "text-xl","p-2","w-96","rounded");
+
+  for (const [format, color] of Object.entries(colors)) {
+    const row = document.createElement("div");
+    const colorValue = document.createElement("span");
+    const icon = document.createElement("i");
+    const img = document.createElement("img");
+
+    // add styles & attributes
+    row.classList.add("opacity-5","hover:opacity-100");
+
+    icon.classList.add(
+      "bg-gray-300",
+      "h-6",
+      "w-6",
+      "rounded",
+      "ml-2",
+      "px-2",
+      "cursor-pointer"
+    );
+    colorValue.classList.add("text-gray-900", "font-bold", "text-xl");
+    icon.classList.add("transition", "duration-150","inline-flex","items-center","justify-center","cursor-pointer","w-8","rounded-full", "px-2");
+    img.src = "./assets/clipboard-outline.svg";
+    colorValue.innerText = color;
+
+    // Copiar valores de los colores
+    icon.addEventListener("click", () => {
+      navigator.clipboard.writeText(color);
+      icon.classList.remove("bg-gray-300");
+      icon.classList.add("bg-green-300");
+
+      setTimeout(() => {
+        icon.classList.remove("bg-green-300");
+        icon.classList.add("bg-gray-300");
+      }, 600);
+    });
+
+    // add elements
+    icon.appendChild(img);
+    row.appendChild(colorValue);
+    row.appendChild(icon);
+    column.appendChild(row);
   }
+  return column;
 }
 
-VendingMachine(20);
+generate.addEventListener("click", async () => {
+  const rangeColors = parseInt(document.getElementById("range-colors").value);
+  const colors = await getRandomColors(rangeColors);
+
+  if (Array.isArray(colors)) {
+    colors.forEach((color) => {
+      const column = ColorValues(color);
+      primary.appendChild(column);
+    });
+  } else {
+    const column = ColorValues(colors);
+    primary.appendChild(column);
+  }
+});
